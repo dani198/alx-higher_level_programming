7e210509626df17793db4580555ce20b9ead051c
dani198 <danyleul16@gmail.com>
2022-09-28 14:40:12 +0300

    idea

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_nums = set(my_list)
-    return sum(unique_nums) 
+    new_list = []
+    result = 0
+
+    if my_list:
+        for elm in my_list[:]:
+            if elm in new_list[:]:
+                continue
+            new_list.append(elm)
+            result += elm
+    return result
