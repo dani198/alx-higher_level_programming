5315791f37141d3d9b134d5efb229fbbaf789c38
dani198 <danyleul16@gmail.com>
2022-10-12 10:24:18 +0300

    higher

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,67 +1,72 @@
 #!/usr/bin/python3
+"""Singley Linked List"""""
+
+
 class Node:
+    """Node class"""
+
     def __init__(self, data, next_node=None):
-        """Defines a node of a singly linked list"""
+        """Defines a node of LL"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """data getter"""
+        """data Getter"""
         return self.__data
 
     @data.setter
     def data(self, value):
-        """data setter"""
-        if type(value) != int:
-            raise TypeError("data must be an integer")
+        """data Setter"""
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-        """next_node getter"""
+        """next_node Getter"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """next_node setter"""
-        if value is not None and type(value) is not Node:
-            raise TypeError("next_node must be a Node object")
+        """next_node Setter"""
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Singley Linked List class"""
+
     def __init__(self):
-        """Defines a singly linked list"""
-        self.__head = None
-
-    def sorted_insert(self, value):
-        new = Node(value)
-        tmp = self.__head
-        add_start = False
-
-        if not self.__head:
-            self.__head = new
-            new.next_node = None
-        else:
-            if value < self.__head.data:
-                add_start = True
-            while tmp.next_node and value > tmp.next_node.data\
-                    and not add_start:
-                tmp = tmp.next_node
-            if not add_start:
-                    new.next_node = tmp.next_node
-                    tmp.next_node = new
-            else:
-                new.next_node = tmp
-                self.__head = new
-            new.data = value
+        """Initialize SSL class"""
+        self.head = None
 
     def __str__(self):
-        s = ""
-        current = self.__head
+        """To string method"""
+        result = ""
+        node = self.head
+        while node:
+            result += str(node.data) + '\n'
+            node = node.next_node
+        return result[:-1]
 
-        while current:
-            s += str(current.data) + '\n'
-            current = current.next_node
-        return s[: -1]
+    def sorted_insert(self, value):
+        """Inserts a new node at sorted position"""
+        new_node = Node(value)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        if value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        node = self.head
+        while node.next_node and node.next_node.data < value:
+            node = node.next_node
+        if node.next_node:
+            new_node.next_node = node.next_node
+        node.next_node = new_node 
